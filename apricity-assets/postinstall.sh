297b1455bc00ca12b9c71070da16d65f25bcb6ef
Alex Gajewski <agajews@gmail.com>
2016-04-30 18:57:51 -0500

    Added init apricity keys

--- apricity-assets/postinstall.sh
+++ apricity-assets/postinstall.sh
@@ -3,3 +3,7 @@ rm /etc/gdm/custom.conf
 USER=$(cat /etc/passwd |grep "/home" |cut -d: -f1)
 usermod -s /bin/zsh $USER
 sed -i /etc/pam.d/su -e 's/.*wheel/#&/'
+pacman-key --init archlinux
+pacman-key --populate archlinux
+pacman-key --init apricity
+pacman-key --populate apricity
