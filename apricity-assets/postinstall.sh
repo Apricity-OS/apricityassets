d47a5d2919d826ca8992cf54fe2498859089c7c3
Alex Gajewski <agajews@gmail.com>
2016-04-30 20:29:59 -0500

    Removed userdel

--- apricity-assets/postinstall.sh
+++ apricity-assets/postinstall.sh
@@ -1,8 +1,7 @@
-userdel liveuser
 rm /etc/gdm/custom.conf
-USER=$(cat /etc/passwd |grep "/home" |cut -d: -f1)
-usermod -s /bin/zsh $USER
-sed -i /etc/pam.d/su -e 's/.*wheel/#&/'
+# USER=$(cat /etc/passwd |grep "/home" |cut -d: -f1)
+# usermod -s /bin/zsh $USER
+# sed -i /etc/pam.d/su -e 's/.*wheel/#&/'
 pacman-key --init archlinux
 pacman-key --populate archlinux
 pacman-key --init apricity
