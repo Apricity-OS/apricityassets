fc18079259c14a975bcfd98736a565c5b72095c7
Alex Gajewski <agajews@gmail.com>
2016-04-30 21:11:19 -0500

    Chsh zsh

--- apricity-assets/postinstall.sh
+++ apricity-assets/postinstall.sh
@@ -1,6 +1,6 @@
 rm -f /etc/gdm/custom.conf
-# USER=$(cat /etc/passwd |grep "/home" |cut -d: -f1)
-# usermod -s /bin/zsh $USER
+USER=$(cat /etc/passwd |grep "/home" |cut -d: -f1)
+usermod -s /bin/zsh $USER
 # sed -i /etc/pam.d/su -e 's/.*wheel/#&/'
 pacman-key --init archlinux
 pacman-key --populate archlinux
