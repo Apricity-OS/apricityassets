118bd6bcf5385260c113c570db41c14ee0e7ac0e
Alex Gajewski <agajews@gmail.com>
2016-07-28 17:20:02 -0500

    Moved extensions, removed redundant code

--- extensions/simple-dock@nothing.org/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-glib-compile-schemas schemas
-
-# msgfmt ./locale/en/LC_MESSAGES/en.po
-# mv ./messages.mo ./locale/en/LC_MESSAGES/simple-dock.mo
-
-# msgfmt ./locale/es/LC_MESSAGES/es.po
-# mv ./messages.mo ./locale/es/LC_MESSAGES/simple-dock.mo
