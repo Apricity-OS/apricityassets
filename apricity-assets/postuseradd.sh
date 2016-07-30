2bf1c3f6827b1c0044db76356a2cf52c6f1d577e
Alex Gajewski <agajews@gmail.com>
2016-07-30 16:40:33 -0500

    Actually fixed postuseradd

--- apricity-assets/postuseradd.sh
+++ apricity-assets/postuseradd.sh
@@ -11,7 +11,7 @@ getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
     echo "$name's home directory is $home"
     top=${home#/}; top=${top%%/*}
     case $top in
-        |bin|dev|etc|lib*|no*|proc|sbin|usr|var) echo "Looks like a system user";;
+        bin|dev|etc|lib*|no*|proc|sbin|usr|var) echo "Looks like a system user";;
         *) sudo -u ${name} freezedry --load /etc/freezedry/default.toml --user;;
     esac
 done
