284ed1575a8484936b3b9c44c0b626d5acf80a81
Alex Gajewski <agajews@gmail.com>
2016-07-30 13:28:36 -0500

    Fixed freezedry postinstall

--- apricity-assets/postinstall.sh
+++ apricity-assets/postinstall.sh
@@ -2,19 +2,3 @@ rm -f /etc/gdm/custom.conf
 USER=$(cat /etc/passwd |grep "/home" |cut -d: -f1)
 usermod -s /bin/zsh $USER
 # sed -i /etc/pam.d/su -e 's/.*wheel/#&/'
-pacman-key --init archlinux
-pacman-key --populate archlinux
-pacman-key --init apricity
-pacman-key --populate apricity
-
-getent passwd | while IFS=: read -r name password uid gid gecos home shell; do
-    echo "$name's home directory is $home"
-    top=${home#/}; top=${top%%/*}
-    case $top in
-        |bin|dev|etc|lib*|no*|proc|sbin|usr|var) echo "Looks like a system user";;
-        *) sudo -u ${name} freezedry --load /etc/freezedry/default.toml --user;;
-    esac
-done
-
-sed -i 's/auth		sufficient	pam_wheel.so trust use_uid/#auth		sufficient	pam_wheel.so trust use_uid/g' /etc/pam.d/su
-yes | freezedry --load /etc/freezedry/default.toml --root
